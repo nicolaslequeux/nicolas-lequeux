e9b940a431c7408023a81e62e408c637d832854e
Nicolas Lequeux <contact@nicolaslequeux.com>
2019-07-16 10:46:32 +0200

    production

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 
 // Reveal Animations When Scrolling
 new WOW().init();
+
+
+// Calculation years-work-finance
+let startingYear = 2008;
+let currentYear = new Date().getFullYear();
+let elapsedTime = currentYear - startingYear;
+let yearsWorkFinance = document.getElementById("years-work-finance");
+yearsWorkFinance.innerHTML = elapsedTime.toString();
