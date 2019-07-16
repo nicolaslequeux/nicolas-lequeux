bf2680f7b2def61c8f0723536b2be1a7a9099132
Nicolas Lequeux <contact@nicolaslequeux.com>
2019-07-16 12:22:47 +0200

    arrow back home

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,23 @@
 new WOW().init();
 
 
+// Close navbar on click outside hamburger button
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+
+//set active navigation after click
+$(".nav-link").on("click", (event) => {
+    $(".navbar-nav").find(".active").removeClass('active');
+    $(event.target).parent().addClass('active');
+  });
+
+  
 // Calculation years-work-finance
 let startingYear = 2008;
 let currentYear = new Date().getFullYear();
 let elapsedTime = currentYear - startingYear;
 let yearsWorkFinance = document.getElementById("years-work-finance");
 yearsWorkFinance.innerHTML = elapsedTime.toString();
+
